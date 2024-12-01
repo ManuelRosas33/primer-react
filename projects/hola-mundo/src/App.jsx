ecc93d0973f1a90a0aad898af1b71e86bb3efa74
Manuel Rosas <mrprg33@gmail.com>
2024-11-30 21:37:38 -0300

    Completamos el ejemplo de twitter

---
+++ projects/hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
+export function App () {
+     
+     return (
+          <section>
+          <TwitterFollowCard  isFollowing userName='red'>
+          Manuel Enrique Rosas
+          </TwitterFollowCard>
+          <TwitterFollowCard isFollowin userName='rdmtn'>
+          Manuel Enrique Rosas
+          </TwitterFollowCard>
+          <TwitterFollowCard  isFollowin userName='arzaffel'>
+          Manuel Enrique Rosas
+          </TwitterFollowCard>
+          </section>
+     )
+}  
